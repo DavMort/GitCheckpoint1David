3b0e03b99c776017bfdf828eabe893832dff07c5
David Mortensen <davidmortensen90@gmail.com>
2023-01-24 13:25:30 +0100

    First version

---
+++ src/CheckTest.java
@@ -0,0 +1,11 @@
+public class CheckTest {
+    private final String printThis;
+
+    public CheckTest(String printThis) {
+        this.printThis = printThis;
+    }
+
+    public void print() {
+        System.out.println(printThis);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the Git checkpoint task 1.1");
+        CheckTest test = new CheckTest("This will be printed when called with the print method in the \"CheckTest\" class");
+
+        test.print();
+    }
+}
