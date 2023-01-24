cd5dcf674cf12d11f9d9806518d6687ec01eaa5d
David Mortensen <davidmortensen90@gmail.com>
2023-01-24 13:37:30 +0100

    Added the Calculate class and a method call in main

---
+++ src/Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+        private final int a;
+        private final int b;
+
+    public Calculate(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int numbersAdded() {
+        return a + b;
+    }
+}
